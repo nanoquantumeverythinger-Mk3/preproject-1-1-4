ba53530ef359dd08537e5819366faaded29e9e60
AndreyKashlaty <15andrey07@gmail.com>
2021-10-29 13:38:13 +0300

    constants

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,38 +10,40 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    //jdbc
-    static private String url = "jdbc:mysql://localhost:3306/my_database?useSSL=false";
-    static private String user = "root";
-    static private String password = "password";
-    static private Connection conn;
+    //constants
+    static private final String URL = "jdbc:mysql://localhost:3306/my_database?useSSL=false";
+    static private final String USER = "root";
+    static private final String PASSWORD = "password";
+    static private final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
+    //jdbc
+    static private Connection conn;
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException cnfe) {
             cnfe.printStackTrace();
         }
-        return conn = DriverManager.getConnection(url, user, password);
+        return conn = DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
     //hibernate
     static SessionFactory sessionFactory;
-
     static {
         Properties prop = new Properties();
-        /*prop.put(Environment.URL, "jdbc:mysql://localhost:3306/my_database?useSSL=false");
-        prop.put(Environment.USER, "root");
-        prop.put(Environment.PASS, "password");
+        /*
+        prop.put(Environment.URL, URL);
+        prop.put(Environment.USER, USER);
+        prop.put(Environment.PASS, PASSWORD);
         prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        prop.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        prop.put(Environment.DRIVER, DRIVER);
         */
-        prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/my_database?serverTimezone=UTC");
+        prop.setProperty("hibernate.connection.url", URL);
         prop.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        prop.setProperty("hibernate.connection.username", "root");
-        prop.setProperty("hibernate.connection.password", "password");
-        prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        prop.setProperty("hibernate.connection.username", USER);
+        prop.setProperty("hibernate.connection.password", PASSWORD);
+        prop.setProperty("hibernate.connection.driver_class", DRIVER);
+
         sessionFactory = new Configuration().addProperties(prop).addAnnotatedClass(User.class).buildSessionFactory();
     }
 
